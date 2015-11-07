b25c4a762c5793cfb4cf2d0fea0fba35797353d2
Ryan Guse <rj2189guse@gmal.com>
2015-11-07 13:21:58 -0600

    Saturday

--- js/script.js
+++ js/script.js
@@ -20,14 +20,27 @@
 
       },
       weeklySpecials : function(){
-      $("#specials p").remove();
-  }
-      // Add a new method to add the weekly specials
+      $("#specials p").remove();        
+      },
+
+//       Add a new method to add the weekly specials
+      $.getJSON("data/specials.json", function (data) {
+      $("#specials p").remove(); 
+      
+      var specials = data.specials,
+      count = specials.length;
+      
+      if(count > 0){
+      $.each(specials, function(i, obj){
+      $("#specials").append(obj.description);
+  });
+}
+ });
 
       // Add a method to validate the registration form
-      // There should be both a name and a valid email
-
-  }
+      // There should  be both a name and a valid email
+}
+ 
 
   // Uncomment the line below to build the menu
   coffeeShopApp.buildMenu();
@@ -36,5 +49,5 @@ coffeeShopApp.weeklySpecials();
   // Call the methods to add the weekly specials
 
   // Add code for the form submit button it should validate the form
- 
+
 }())
