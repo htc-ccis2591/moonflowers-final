21c798fc4cc6bc61c30845393f36591691c0db97
Tim Mechtel <tim_mechtel@hotmail.com>
2015-11-11 19:47:54 -0600

    it works

--- js/script.js
+++ js/script.js
@@ -17,51 +17,75 @@
         }
     };
     var MenuItems = {
-    BuildItems: function (data) {
-        $.ajax({
-        type: 'get',
-        url: 'data/menu.json',
-        success: function (data2) {
-            var menus = data2.menu,
-                count = menus.length;
-            if (count > 0) {
-                $.each(menus, function (i, obj) {
-                    if (obj.type === 'beverage') {
-                        var img = $('img').attr("src", obj.img)
-                        $("#espresso").append("<li>" + "<span class=menu-item>" + obj.name + "<img src=" + img + ">" + obj.price);
-                        }
-                    if (obj.type === 'bakery') {
-                     // var pic = JSON.parse(obj.img);
-                        var img2 = $('img').attr("src", obj.img)
-                        $("#bakery").append("<li>" + "<span class=menu-item>" + obj.name + "<img src=" + img2 + ">" + obj.price)
-                    };
-                });
-            };
-        }
-    });
-    }
-    }
-    // Add a new method to add the weekly specials
-   var SpecialItems = { 
-    AllSpecial: function (data){
-        $.ajax({
-        type: 'GET',
-        url: 'data/specials.json',
-        dataType: 'json',
-        success: function (data) {
-            var special = data.specials,
-                count = special.length;
-            if (count > 0) {
-                $.each(special, function (i, obj) {
-                   // $("#specials").append("<p>" + obj.key);
-                    $("#specials").append("<p>" + obj.description);
-                });
-            };
-        }
+        BuildItems: function (data) {
+            $.ajax({
+                type: 'get',
+                url: 'data/menu.json',
+                success: function (data2) {
+                    var menus = data2.menu,
+                        count = menus.length;
+                    if (count > 0) {
+                        $.each(menus, function (i, obj) {
 
-    });
-}
-   }
+                            if (obj.type === 'beverage') {
+                                var li = $("<li>");
+                                $("#espresso").append(li);
+                                li.append("<span class=menu-item>" + obj.name + " " + obj.price);
+                                if (obj.img !== undefined) {
+                                    li.append("<img src=" + obj.img + ">");
+                                }
+                            }
+                            if (obj.type === 'bakery') {
+                                var li2 = $("<li>");
+
+                                $("#bakery").append(li2)
+                                li2.append("<span class=menu-item>" + obj.name + " " + obj.price);
+                                if (obj.img !== undefined) {
+                                    li2.append("<img src=" + obj.img + ">");
+                                    $("img").attr("class", "hide");
+                                    $(".menu-item").click(function () {
+                                        var pic = $(this).next().attr("src");
+                                        console.log($(this).next())
+                                        $("#aside-image").attr("src", pic).removeAttr("class");
+                                    })
+                                }
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    }
+
+
+
+
+
+
+
+
+
+    // Add a new method to add the weekly specials
+    var SpecialItems = {
+        AllSpecial: function (data) {
+            $.ajax({
+                type: 'GET',
+                url: 'data/specials.json',
+                dataType: 'json',
+                success: function (data) {
+                    var special = data.specials,
+                        count = special.length;
+                    if (count > 0) {
+                        $.each(special, function (i, obj) {
+                            // $("#specials").append("<p>" + obj.key);
+                            $("#specials").append("<p>" + obj.description);
+                        });
+                    };
+                }
+
+            });
+        }
+    }
 
     // Add a method to validate the registration form
     // There should be both a name and a valid email
@@ -76,14 +100,18 @@
     SpecialItems.AllSpecial();
 
     // Add code for the form submit button it should validate the form
-   
+
     $("#register").submit(function () {
+        var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 
         var text = $("#email").val();
         console.log(text);
 
         if (text === "") {
             alert("Email can't be blank!");
+        }
+        if (!filter.test(text)) {
+            alert("not good");
         }
 
         var name = $("#name").val();
@@ -95,5 +123,5 @@
         //event.preventDefault();
         return false;
     });
-    
+
 }())
